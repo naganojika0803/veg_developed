d54766b99fc0329a16e633b6e752290e21a93d35
naganojika0803 <v16kashima@gmail.com>
2023-12-17 19:12:49 +0900

    デプロイ用の変更

--- veg_project/settings.py
+++ veg_project/settings.py
@@ -12,19 +12,20 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zx%8aru4u@70r0^3r70py!*!e3c3oj_8zzl&l($mr#$pf=ju0m'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# ローカル環境で行う際は以下を使用（デプロイ時はコメントアウト）
+# DEBUG = True
+# デプロイ時は以下を使用します
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
+
 
 ALLOWED_HOSTS = []
 
@@ -75,10 +76,12 @@ WSGI_APPLICATION = 'veg_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# デプロイ時は、ENGINEとNAMEをコメントアウトにします
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': env.db(),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
